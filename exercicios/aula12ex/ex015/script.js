2bb513bb0f67d220c0b5284aad218474ca3efad6
Eraldo Albano <albanoeraldo.ti@gmail.com>
2025-10-28 14:18:25 -0300

    ex015 top

---
+++ exercicios/aula12ex/ex015/script.js
@@ -0,0 +1,51 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtdata')
+    var res = window.document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano ) {
+        window.alert('ERRO - Verifique os dados e tente novamente - ERRO')
+    } else {
+        var fsex = window.document.getElementsByName('sexo')
+        var idade = ano - fano.value
+        var genero = ''
+        var img = window.document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+               // criança
+                img.setAttribute('src', 'criancaH.jpg')
+            } else if (idade < 18) {
+                //jovem
+                img.setAttribute('src', 'jovemH.jpg')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adultoH.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosoH.jpg')
+            }
+                
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+               // criança
+               img.setAttribute('src', 'criancaM.jpg')
+            } else if (idade < 18) {
+                //jovem
+                img.setAttribute('src', 'jovemM.jpg')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adultoM.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosoM.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+        
+}
